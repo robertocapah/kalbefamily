344108acde0beb0113c38da65c53a0bed20b2daa
Rian Andrivani <rianandrivani@gmail.com>
2017-12-13 09:53:03 +0700

    update api di mConfig

--- app/src/main/java/kalbefamily/crm/kalbe/kalbefamily/Repo/mConfigRepo.java
+++ app/src/main/java/kalbefamily/crm/kalbe/kalbefamily/Repo/mConfigRepo.java
@@ -15,7 +15,8 @@ import kalbefamily.crm.kalbe.kalbefamily.Data.DatabaseManager;
 
 public class mConfigRepo {
     DatabaseHelper helper;
-    public String API = "http://10.171.11.86/WebApi2/KF/";
+//    public String API = "http://10.171.11.114/WebApi2/KF/";
+    public String API = "http://Apikf.kalbenutritionals.web.id/KF/";
 
     public mConfigRepo(Context context) {
         DatabaseManager.init(context);
@@ -40,18 +41,18 @@ public class mConfigRepo {
         data1.setTxtDefaultValue("5");
         data1.setIntEditAdmin("1");
         helper.getmConfigDao().createOrUpdate(data1);
-        mConfigData data2 = new mConfigData();
-        data2.setIntId(2);
-        data2.setTxtName("API_LOGIN");
-        data2.setTxtValue("http://http://10.171.11.70/WebApi2/KF/GetDetailKontak");
-        data2.setTxtDefaultValue("http://10.171.11.70/WebApi2/KF/GetDetailKontak");
-        data2.setIntEditAdmin("1");
-        helper.getmConfigDao().createOrUpdate(data2);
+//        mConfigData data2 = new mConfigData();
+//        data2.setIntId(2);
+//        data2.setTxtName("API_LOGIN");
+//        data2.setTxtValue("http://http://10.171.11.70/WebApi2/KF/GetDetailKontak");
+//        data2.setTxtDefaultValue("http://10.171.11.70/WebApi2/KF/GetDetailKontak");
+//        data2.setIntEditAdmin("1");
+//        helper.getmConfigDao().createOrUpdate(data2);
         mConfigData data3 = new mConfigData();
         data3.setIntId(3);
         data3.setTxtName("API_VERSION");
-        data3.setTxtValue("http://http://10.171.11.70/WebApi2/KF/CheckVersionApp");
-        data3.setTxtDefaultValue("http://10.171.11.70/WebApi2/KF/CheckVersionApp");
+        data3.setTxtValue(API);
+        data3.setTxtDefaultValue(API);
         data3.setIntEditAdmin("1");
         helper.getmConfigDao().createOrUpdate(data3);
         mConfigData data4 = new mConfigData();
